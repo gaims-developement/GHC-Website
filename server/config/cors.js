8d47b801082cc92984d802407c2411e28e1ab8cb
Sushmit Morey <sushmitmorey1805@gmail.com>
2026-06-19 01:03:00 +0530

    ISTG THIS SHOULD WORK NOW :CRY

--- server/config/cors.js
+++ server/config/cors.js
@@ -23,7 +23,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
   optionsSuccessStatus: 204,
 };
 
